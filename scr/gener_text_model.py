6addab437326f7fec49cca49cab1f511e17c4f21
GitAlexandr <sashabushuev03@mail.ru>
2023-12-08 15:05:35 +0300

    work_generator

--- scr/gener_text_model.py
+++ scr/gener_text_model.py
@@ -1,47 +1,73 @@
-# from YaGPT import YaGPT, YaGPTException
+import requests
 
-# # Replace with your actual values
-# folder_id = "b1gjocqmnjl20h2d2528"
-# iam_token = "y0_AgAAAABE5ACHAATuwQAAAADe37C6dCOE7pouSmerwnJ8iQXmdIUjbZ8"
+url = "https://chatgpt-42.p.rapidapi.com/gpt4"
 
-# # Create a LanguageModel instance
-# lm = YaGPT(folder_id, iam_token)
+message = "Ответственный исполнитель должен получить до представления на подпись (утверждение) проекта документа его официальное согласование с должностными лицами (руководителями подразделений общества), в ведении которых находятся вопросы, изложенные в проекте. Согласование проекта осуществляется путем направления его по маршруту «Согласование», сформированному в ЕАСД. Круг согласующих не следует без надобности расширять."
 
-# try:
-#     result = lm.instruct(
-#             model="general",
-#             instruction_text="Найди ошибки в тексте и исправь их",
-#             request_text="Ламинат подойдет для укладке на кухне или в детской комнате",
-#             max_tokens=1500,
-#             temperature=0.6)
+first_message = f"Задай краткий вопрос по следующему тексту:\n{message}"
 
-#     if result:
-#         for alternative in result:
-#             print(f"Generated Text: {alternative['text']}")
-# except YaGPTException as e:
-#     print(f"Language Model Error: {e}")
+second_message = f"Переформулируй тремя различными способами следующий вопрос и напиши это списком:\n{first_message}"
 
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
+third_message = f"Сократи до 7 слов следующий текст:\n{message}"
 
-# driver = webdriver.Chrome()
-
-# driver.get("https://www.phind.com/")
-
-# title = driver.title
-
-# driver.implicitly_wait(0.5)
-
-# text_box = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/main/div/div/div/div[1]/div[2]/div/form/div[1]/textarea')
-# submit_button = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/main/div/div/div/div[1]/div[2]/div/form/div[1]/div[2]/button[2]')
-
-# text_box.send_keys("Hi")
-# submit_button.click()
-
-# message = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/main/div/div[2]/div[4]')
-# text = message.text
-
-# driver.quit()
+fourth_message = f"Переформулируй данный текст:\n{third_message}"
 
 
-																					
+questions = [first_message, second_message, third_message, fourth_message]
+
+payloads = [
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": question
+            }
+        ],
+        "tone": "Balanced"
+    }
+    for question in questions
+]
+headers = {
+	"content-type": "application/json",
+	"X-RapidAPI-Key": "0f4fc91112mshac6ca50c42a20fcp18e4bajsn3e4fb2d0f8cf",
+	"X-RapidAPI-Host": "chatgpt-42.p.rapidapi.com"
+}
+
+list_response = []
+
+for payload in payloads:
+    response = requests.post(url, json=payload, headers=headers)
+    result = response.json()['result']
+    list_response.append(result)
+    print(result)
+
+main_theme = "ТЕМА"	
+question = f"{list_response[0]}"
+questions_paraphrase = f"{list_response[1]}"	
+answer = f"{message}"
+answer_summary = f"{list_response[2]}"
+answer_paraphrase = f"{list_response[3]}"
+intent = "1"
+answers_merged = f"[{answer},{answer_summary},{answer_paraphrase}]"
+questions_merged = f"[{question},{questions_paraphrase}]"
+
+import pandas as pd
+
+data = {
+    'main_theme': [main_theme],
+    'question': [question],
+    'questions_paraphrase': [questions_paraphrase],
+    'answer': [answer],
+    'answer_summary': [answer_summary],
+    'answer_paraphrase': [answer_paraphrase],
+    'intent': [intent],
+    'answers_merged': [answers_merged],
+    'questions_merged': [questions_merged]
+}
+
+df = pd.DataFrame(data)
+
+xlsx_file = 'output.xlsx'
+df.to_excel(xlsx_file, index=False)
+
+print(f"Data written to {xlsx_file}")
