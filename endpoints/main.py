04c0ca019f69d129910ff9b5b94385567f9876cd
GitAlexandr <sashabushuev03@mail.ru>
2023-12-21 17:25:25 +0300

    dsfdasdf

--- endpoints/main.py
+++ endpoints/main.py
@@ -2,8 +2,23 @@ from fastapi import FastAPI, Request, HTTPException
 from pydantic import BaseModel
 from rasa.core.agent import Agent
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Message(BaseModel):
     message: str
